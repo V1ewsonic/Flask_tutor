d9aa0ae367fcaecb6359682442b9b798b1ac372a
V1ewsonic <fanka730@gmail.com>
2023-02-07 17:49:58 +0700

    up : mengganti nama DB

--- routes.py
+++ routes.py
@@ -9,7 +9,7 @@ app.secret_key = "asdfghjkl12345fdsa_fdsakld8rweodfds"
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'test'
+app.config['MYSQL_DB'] = 'universitas'
 mysql = MySQL(app)
 
 # ROUTING
